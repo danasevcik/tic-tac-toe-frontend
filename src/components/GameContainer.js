24f509c5ead17e859b84bb74b462d9c0d41f25e1
github username <danasevcik@hotmail.com>
2019-06-27 16:13:56 -0400

    formatted tic-tac-toe board

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -4,9 +4,13 @@ class GameContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="board-container">
         <h1>GAME CONTAINER</h1>
+        <div id="board">
+          <div>
 
+          </div>
+        </div>
 
 
       </div>
