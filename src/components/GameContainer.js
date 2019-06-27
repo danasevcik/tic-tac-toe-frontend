68a5987bebb4b76e7108e9b4ce30e09bca0680fd
github username <danasevcik@hotmail.com>
2019-06-27 16:18:45 -0400

    border for tic tac toe board

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 class GameContainer extends React.Component {
 
+  state = {
+    user: null
+  }
+
   render() {
     return (
       <div id="board-container">
-        <h1>GAME CONTAINER</h1>
+        {this.state.user ? <h1>Welcome, {this.state.user}</h1> : <h1>Welcome</h1>}
         <div id="board">
-          <div>
-
-          </div>
+          <div class="spot" id='1'></div>
+          <div class="spot" id='2'></div>
+          <div class="spot" id='3'></div>
+          <div class="spot" id='4'></div>
+          <div class="spot" id='5'></div>
+          <div class="spot" id='6'></div>
+          <div class="spot" id='7'></div>
+          <div class="spot" id='8'></div>
+          <div class="spot" id='9'></div>
         </div>
 
 
