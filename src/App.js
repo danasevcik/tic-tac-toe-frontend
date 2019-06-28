cf7445e2e164d67ebc1a53f4924ff670dee184c6
Dana Sevcik <danasevcik@hotmail.com>
2019-06-28 09:49:21 -0400

    start button functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import './App.css';
 import GameContainer from './components/GameContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <GameContainer />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    start: false
+  }
+
+  startGame = () => {
+    this.setState({start: true})
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.start ?
+          <GameContainer />
+          :
+          <div>
+            <h1>Welcome</h1>
+            <button onClick={() => {this.startGame()}}>Start Game</button>
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 
