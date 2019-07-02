09560c1570fbf0e20b3046511d69a28bee12450c
Dana Sevcik <danasevcik@hotmail.com>
2019-07-02 09:58:06 -0400

    functionality for new and returning users

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,14 +5,16 @@ class Login extends React.Component {
   state = {
     name: null,
     hometown: null,
-    funFact: null
+    funFact: null,
+    new: false,
+    old: false
   }
 
   handleChange = (e) => {
     this.setState({name: e.target.value})
   }
 
-  handleLogin = (state) => {
+  handleCreate = (state) => {
     fetch(`http://localhost:3000/users`, {
       method: "POST",
       headers: {
@@ -29,16 +31,61 @@ class Login extends React.Component {
     })
   }
 
+  handleLogin = (state) => {
+    fetch(`http://localhost:3000/find-user`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        accepts: "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          name: state.name
+        }
+      })
+    })
+  }
+
+  handleNew = () => {
+    this.setState({new: true})
+  }
+
+  handleOld = () => {
+    this.setState({old: true})
+  }
+
+  goBack = () => {
+    this.setState({new: false})
+    this.setState({old: false})
+  }
+
   render() {
     console.log('rendered');
     return (
       <div>
-        <input onChange={(e) => {this.handleChange(e)}} name="name" placeholder="name"></input>
-        <input onChange={(e) => {this.handleChange(e)}} name="hometown" placeholder="hometown"></input>
-        <input onChange={(e) => {this.handleChange(e)}} name="funFact" placeholder="fun fact"></input>
-        <br></br>
-        <button onClick={() => {this.handleLogin(this.state)}}>Let's Get Started</button>
-      </div>
+        {(this.state.new || this.state.old) ? null :
+          <button onClick={() => {this.handleNew()}}>New User</button>
+        }
+        {(this.state.new || this.state.old) ? null :
+          <button onClick={() => {this.handleOld()}}>Returning User</button>
+        }
+      {this.state.new &&
+        <div>
+          <input onChange={(e) => {this.handleChange(e)}} name="name" placeholder="name"></input>
+          <input onChange={(e) => {this.handleChange(e)}} name="hometown" placeholder="hometown"></input>
+          <input onChange={(e) => {this.handleChange(e)}} name="funFact" placeholder="fun fact"></input>
+          <button onClick={() => {this.handleCreate(this.state)}}>Let's Get Started</button>
+          <button onClick={() => {this.goBack()}}>Back</button>
+        </div>
+      }
+      {this.state.old &&
+        <div>
+          <input onChange={(e) => {this.handleChange(e)}} name="name" placeholder="name"></input>
+          <button onClick={() => {this.handleLogin(this.state)}}>Let's Get Started</button>
+          <button onClick={() => {this.goBack()}}>Back</button>
+        </div>
+      }
+    </div>
     )
   }
 }
