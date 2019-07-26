24f2448ef4d1bcfc79b91a592ceab875939cdc1b
Dana Sevcik <danasevcik@hotmail.com>
2019-07-26 10:42:42 -0400

    block user from winning with 0 and 8 starting position

--- src/components/EasyCompGameContainer.js
+++ src/components/EasyCompGameContainer.js
@@ -236,6 +236,13 @@ class EasyCompGameContainer extends React.Component {
           }
         }
       }
+      if (this.state.game[0] === 'x' && this.state.game[8] === 'x') {
+        if (this.state.game[2] === null && this.state.currentPlayer === 'o') {
+          this.state.game[2] = this.state.currentPlayer
+          this.setState({currentPlayer: 'x'})
+          this.setState({game: [...this.state.game]})
+        }
+      }
 
 
     }
