ffa40b139831de5e6221a66859d564a0e858d007
Dana Sevcik <danasevcik@hotmail.com>
2019-08-01 11:17:38 -0400

    next move for computer in position 2

--- src/components/EasyCompGameContainer.js
+++ src/components/EasyCompGameContainer.js
@@ -359,6 +359,13 @@ class EasyCompGameContainer extends React.Component {
           }
         }
       }
+      if (this.state.game[3] === 'x' && this.state.game[5] === 'x') {
+        if (this.state.game[2] === null && this.state.currentPlayer === 'o') {
+          this.state.game[2] = this.state.currentPlayer
+          this.setState({currentPlayer: 'x'})
+          this.setState({game: [...this.state.game]})
+        }
+      }
 
     }
     , 2000)
